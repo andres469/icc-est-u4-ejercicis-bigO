49bcf179d808e23a1765b0ea4747e0bf67839510
Andres Tenesaca <jtenesaca74@gmail.com>
2025-05-20 19:53:07 -0500

    first comit

--- src/main/ejercicio1/AlgoritmosEjemplo.java
+++ src/main/ejercicio1/AlgoritmosEjemplo.java
@@ -48,7 +48,7 @@ public class AlgoritmosEjemplo {
     }
 
     // 6. Bubble Sort
-    public static void bubbleSort(int[] arr) {
+    public static void bubbleSort(int[] arr) { 
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
--- src/main/ejercicio1/RespuestasComplejidad.java
+++ src/main/ejercicio1/RespuestasComplejidad.java
@@ -2,14 +2,14 @@ package main.ejercicio1;
 
 public class RespuestasComplejidad {
     // Los estudiantes deben completar cada variable con la notación Big O correcta
-    public static String imprimirArreglo = ""; // 1
-    public static String busquedaLineal = ""; // 2
-    public static String busquedaBinaria = ""; // 3
-    public static String factorial = ""; // 4
-    public static String fibonacci = ""; // 5
-    public static String bubbleSort = ""; // 6
-    public static String quickSort = ""; // 7
-    public static String sumaMatriz = ""; // 8
-    public static String encontrarMaximo = ""; // 9
-    public static String potencia = ""; // 10
+    public static String imprimirArreglo = "o(n)"; // 1
+    public static String busquedaLineal = "o(n)"; // 2
+    public static String busquedaBinaria = "o (log n)"; // 3
+    public static String factorial = "o(n)"; // 4
+    public static String fibonacci = "o(2^n)"; // 5
+    public static String bubbleSort = "o(n^2)"; // 6
+    public static String quickSort = "o(n log n)"; // 7
+    public static String sumaMatriz = "o(n^2)"; // 8
+    public static String encontrarMaximo = "o(n)"; // 9
+    public static String potencia = "o(n)"; // 10
 }
